4c52d5f1c3723c1b1fbb8f5db2b1ca6c7dad2542
Aluminium <aluminium@dsq-group.ru>
2024-07-24 16:36:09 +0300

    1.0.1 shulker fix

--- src/main/java/net/aluminiumtn/shiftitems/AutoCollectHandler.java
+++ src/main/java/net/aluminiumtn/shiftitems/AutoCollectHandler.java
@@ -3,11 +3,14 @@ package net.aluminiumtn.shiftitems;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.block.entity.BlockEntity;
 
 public class AutoCollectHandler {
     public static void register() {
@@ -18,23 +21,33 @@ public class AutoCollectHandler {
         if (!world.isClient) {
             if (player.isSneaking()) {
                 Block block = state.getBlock();
-                ItemStack stack = new ItemStack(block.asItem());
 
-                if (!stack.isEmpty()) {
+                if (block instanceof ShulkerBoxBlock && blockEntity instanceof ShulkerBoxBlockEntity) {
+                    ItemStack shulkerStack = new ItemStack(block);
+                    ShulkerBoxBlockEntity shulkerBoxEntity = (ShulkerBoxBlockEntity) blockEntity;
+                    NbtCompound shulkerTag = shulkerBoxEntity.createNbt();
 
-                    if (!player.getInventory().insertStack(stack)) {
-                        player.dropItem(stack, false); 
+                    shulkerStack.setSubNbt("BlockEntityTag", shulkerTag);
+
+                    if (!player.getInventory().insertStack(shulkerStack)) {
+                        player.dropItem(shulkerStack, false);
                     }
 
-             
-                    world.setBlockState(pos, state.getFluidState().getBlockState(), 3); 
-                    
-           
-                    return false; 
+                    world.setBlockState(pos, state.getFluidState().getBlockState(), 3);
+
+                    return false;
+                }
+
+                ItemStack stack = new ItemStack(block.asItem());
+                if (!stack.isEmpty()) {
+                    if (!player.getInventory().insertStack(stack)) {
+                        player.dropItem(stack, false);
+                    }
+                    world.setBlockState(pos, state.getFluidState().getBlockState(), 3);
+                    return false;
                 }
             }
         }
-        return true; 
+        return true;
     }
 }
-
